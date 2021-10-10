badf78288a1aae35ec2a1e84b9bfffa3f07c8dfd
Veranis Nuñez <Veranisnb@gmail.com>
2021-10-09 22:15:35 -0500

    user-profile

--- app.py
+++ app.py
@@ -22,5 +22,16 @@ def admin():
     return render_template("admin.html")
 
 
+@app.route("/user/")
+@app.route("/user/profile/")
+def userProfile():
+    return render_template("userProfile.html", title='Perfil')
+
+
+@app.route("/user/feedback/")
+def userRetroalimentacion():
+    return render_template("userRetro.html", title='Retroalimentación')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
