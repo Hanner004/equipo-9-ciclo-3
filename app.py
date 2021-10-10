19720add6906d18bf85f992836570f5276087a40
Hanner004 <hanerdelahoz900@hotmail.com>
2021-10-09 19:48:57 -0500

    feature(dev): register

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 
 @app.route("/login")
 def login():
