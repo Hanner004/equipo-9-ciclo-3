0f3c98ce6fb402a1e5a3210122a51a5d04880f44
Hanner004 <hanerdelahoz900@hotmail.com>
2021-10-15 17:43:42 -0500

    Registro y inicio de sesión

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.secret_key = "123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Users(db.Model):
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String)
+    phone = db.Column(db.Integer)
+    direction = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return "Usuario registrado" + str(self.id)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 @app.route("/")
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        return redirect("/user")
+        email = request.form["email"]
+        password = request.form["password"]
+        user = Users.query.filter_by(email=email).first()
+
+        if not user or not check_password_hash(user.password, password):
+            #ERROR: El inicio de sesión o la contraseña no son válidos
+            return render_template("login.html", title="Iniciar sesión")
+
+        return render_template("admin.html")
+
     else:
         return render_template("login.html", title="Iniciar sesión")
 
@@ -16,7 +53,25 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        return redirect("/login")
+        fullname = request.form["fullname"]
+        phone = request.form["phone"]
+        direction = request.form["direction"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm = request.form["confirm"]
+
+        if password == confirm:
+            user = Users(fullname=fullname, phone=phone, direction=direction, email=email)
+            user.set_password(password)
+            db.session.add(user)
+            db.session.commit()
+
+            flash("Usuario registrado")
+            return render_template('login.html')
+
+        else:
+            return render_template("register.html", title="Registro")
+            
     else:
         return render_template("register.html", title="Registro")
 
