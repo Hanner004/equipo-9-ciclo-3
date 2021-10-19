57aeef1eb885b5c90eb834f40164c89c58189d83
Hanner004 <hanerdelahoz900@hotmail.com>
2021-10-19 13:43:30 -0500

    Validaciones

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import redirect
+from markupsafe import escape
 
 app = Flask(__name__)
 app.secret_key = "123"
@@ -23,7 +24,7 @@ class Users(db.Model):
     password = db.Column(db.String)
 
     def __repr__(self):
-        return "Usuario registrado" + str(self.id)
+        return "Usuario registrado " + str(self.id)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -32,56 +33,84 @@ class Users(db.Model):
         return check_password_hash(self.password, password)
 
 
-@app.route("/")
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        user = Users.query.filter_by(email=email).first()
+@app.route("/", methods=["GET"])
+def index():
+    return redirect("/login/")
 
-        if not user or not check_password_hash(user.password, password):
-            #ERROR: El inicio de sesión o la contraseña no son válidos
-            return render_template("login.html", title="Iniciar sesión")
 
-        return render_template("admin.html")
+@app.route("/login/", methods=["GET"])
+def getLogin():
+    return render_template("login.html", title="Iniciar sesión")
 
-    else:
+
+@app.route("/login/", methods=["POST"])
+def postLogin():
+    email = escape(request.form["email"].strip())
+    password = escape(request.form["password"].strip())
+    if email == None or len(email) == 0 or password == None or len(password) == 0:
+        flash("ERROR: Rellenar todos los campos")
         return render_template("login.html", title="Iniciar sesión")
+    else:
+        user = Users.query.filter_by(email=email).first()
+        if check_password_hash(user.password, password) != True:
+            flash("El inicio de sesión o la contraseña no son válidos")
+            return render_template("login.html", title="Iniciar sesión")
+        else:
+            flash(f"Bienvenido {user.fullname}")
+            return redirect("/dashboard/")
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register/", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        fullname = request.form["fullname"]
-        phone = request.form["phone"]
-        direction = request.form["direction"]
-        email = request.form["email"]
-        password = request.form["password"]
-        confirm = request.form["confirm"]
-
-        if password == confirm:
-            user = Users(fullname=fullname, phone=phone, direction=direction, email=email)
-            user.set_password(password)
-            db.session.add(user)
-            db.session.commit()
-
-            flash("Usuario registrado")
-            return render_template('login.html')
-
+        isValid = True
+        fullname = escape(request.form["fullname"].strip())
+        phone = escape(request.form["phone"].strip())
+        direction = escape(request.form["direction"].strip())
+        email = escape(request.form["email"].strip())
+        password = escape(request.form["password"].strip())
+        confirm = escape(request.form["confirm"].strip())
+        if fullname == None or len(fullname) == 0:
+            isValid = False
+        if phone == None or len(phone) == 0:
+            isValid = False
+        if direction == None or len(direction) == 0:
+            isValid = False
+        if email == None or len(email) == 0:
+            isValid = False
+        if password == None or len(password) == 0:
+            isValid = False
+        if confirm == None or len(confirm) == 0:
+            isValid = False
+        if isValid:
+            if password == confirm:
+                user = Users(fullname=fullname, phone=phone, direction=direction, email=email)
+                user.set_password(password)
+                db.session.add(user)
+                db.session.commit()
+                flash("Usuario registrado")
+                return render_template('login.html')
+            else:
+                flash("Las contraseñas no coinciden")
+                return render_template("register.html", title="Registro")
         else:
+            flash("ERROR: Rellenar todos los campos")
             return render_template("register.html", title="Registro")
-            
     else:
         return render_template("register.html", title="Registro")
 
 
-@app.route("/users", methods=["GET"])
+@app.route("/dashboard/", methods=["GET"])
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/users/", methods=["GET"])
 def users():
     return "USERS"
 
 
-@app.route("/users/<int:id>", methods=["GET", "POST", "PUT", "DELETE"])
+@app.route("/users/<int:id>/", methods=["GET", "POST", "PUT", "DELETE"])
 def usersCRUD(id):
     if request.method == "GET":
         return f"USER {id}"
