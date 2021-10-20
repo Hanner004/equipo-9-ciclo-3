a52916fab8bcdb36d7f75f85377d140f0993e083
Hanner004 <hanerdelahoz900@hotmail.com>
2021-10-19 20:31:29 -0500

    dashboard superadmin

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def postLogin():
             return render_template("login.html", title="Iniciar sesión")
         else:
             flash(f"Bienvenido {user.fullname}")
-            return redirect("/dashboard/")
+            return redirect("/superadmin/")
 
 
 @app.route("/register/", methods=["GET", "POST"])
@@ -100,52 +100,52 @@ def register():
         return render_template("register.html", title="Registro")
 
 
-@app.route("/dashboard/", methods=["GET"])
-def dashboard():
-    return render_template("dashboard.html")
+@app.route("/<string:role>/", methods=["GET"])
+def dashboard(role):
+    return render_template("dashboard.html", role = role)
 
 
-@app.route("/users/", methods=["GET"])
-def users():
-    return "USERS"
+# @app.route("/users/", methods=["GET"])
+# def users():
+#     return "USERS"
 
 
-@app.route("/users/<int:id>/", methods=["GET", "POST", "PUT", "DELETE"])
-def usersCRUD(id):
-    if request.method == "GET":
-        return f"USER {id}"
-    elif request.method == "POST":
-        return f"USER {id} CREATED"
-    elif request.method == "PUT":
-        return f"USER {id} UPDATED"
-    elif request.method == "DELETE":
-        return f"USER {id} DELETED"
+# @app.route("/users/<int:id>/", methods=["GET", "POST", "PUT", "DELETE"])
+# def usersCRUD(id):
+#     if request.method == "GET":
+#         return f"USER {id}"
+#     elif request.method == "POST":
+#         return f"USER {id} CREATED"
+#     elif request.method == "PUT":
+#         return f"USER {id} UPDATED"
+#     elif request.method == "DELETE":
+#         return f"USER {id} DELETED"
 
 
-@app.route("/admin/")
-def admin():
-    return render_template("admin.html")
+# @app.route("/admin/")
+# def admin():
+#     return render_template("admin.html")
 
 
-@app.route("/admin/createemployee/")
-def adminCreateEmployee():
-    return render_template("adminCreateEmployee.html", title='CrearEmpleado')
+# @app.route("/admin/createemployee/")
+# def adminCreateEmployee():
+#     return render_template("adminCreateEmployee.html", title='CrearEmpleado')
 
 
-@app.route("/user/")
-@app.route("/user/profile/")
-def userProfile():
-    return render_template("userProfile.html", title='Perfil')
+# @app.route("/user/")
+# @app.route("/user/profile/")
+# def userProfile():
+#     return render_template("userProfile.html", title='Perfil')
 
 
-@app.route("/user/id/")
-def editUser():
-    return render_template("editUser.html", title='Editar usuario')
+# @app.route("/user/id/")
+# def editUser():
+#     return render_template("editUser.html", title='Editar usuario')
 
 
-@app.route("/user/feedback/")
-def userFeedback():
-    return render_template("userFeedback.html", title='Retroalimentación')
+# @app.route("/user/feedback/")
+# def userFeedback():
+#     return render_template("userFeedback.html", title='Retroalimentación')
 
 
 if __name__ == "__main__":
